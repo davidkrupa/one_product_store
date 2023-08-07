90e9f7b2347308906b9f608e8c94ece681bc06c7
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2023-08-07 21:35:23 +0200

    useTimer custom hook

--- components/Timer.jsx
+++ components/Timer.jsx
@@ -1,31 +1,56 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { useTimer } from "@/hooks/useTimer";
+
+const END_DATE = new Date("Mon Aug 07 2024 20:11:38 GMT+0200");
 
 const Timer = () => {
-  const timer = {
+  const [days, hours, minutes, seconds] = useTimer(END_DATE);
+
+  console.log(seconds);
+
+  const timerTest = {
     days: 0,
     hours: 10,
     mins: 20,
     secs: 40,
   };
 
-  const timerKeys = Object.keys(timer);
+  const timerKeys = Object.keys(timerTest);
   const timerUpperCase = timerKeys.map(
     (el) => el.charAt(0).toUpperCase() + el.slice(1)
   );
 
   return (
     <div className="flex gap-4">
-      {timerUpperCase.map((item, index) => (
+      {/* {timerUpperCase.map((item, index) => (
         <div
           key={item}
           className="flex flex-col justify-center items-center p-4 rounded-xl bg-slate-600"
         >
-          <p className="text-2xl font-semibold">{timer[timerKeys[index]]}</p>
+          <p className="text-2xl font-semibold">
+            {timerTest[timerKeys[index]]}
+          </p>
           <p className="text-sm">{item}</p>
         </div>
-      ))}
+      ))} */}
+      <div className="flex flex-col justify-center items-center p-4 rounded-xl bg-slate-600">
+        <p className="text-2xl font-semibold">{days}</p>
+        <p className="text-sm">Days</p>
+      </div>
+      <div className="flex flex-col justify-center items-center p-4 rounded-xl bg-slate-600">
+        <p className="text-2xl font-semibold">{hours}</p>
+        <p className="text-sm">Hours</p>
+      </div>
+      <div className="flex flex-col justify-center items-center p-4 rounded-xl bg-slate-600">
+        <p className="text-2xl font-semibold">{minutes}</p>
+        <p className="text-sm">Minutes</p>
+      </div>
+      <div className="flex flex-col justify-center items-center p-4 rounded-xl bg-slate-600">
+        <p className="text-2xl font-semibold">{seconds}</p>
+        <p className="text-sm">Seconds</p>
+      </div>
     </div>
   );
 };
---
+++ hooks/useTimer.js
@@ -0,0 +1,36 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export const useTimer = (endDate) => {
+  const [timer, setTimer] = useState(() => getTimeLeft(endDate));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer(getTimeLeft(endDate));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [endDate]);
+
+  return getFormattedTimeLeft(timer);
+};
+
+const getTimeLeft = (endDate) => {
+  return endDate.getTime() - new Date().getTime();
+};
+
+const getFormattedTimeLeft = (timer) => {
+  const miliseconds = {
+    day: 86400000, // 1000 * 60 * 60 * 24
+    hour: 3600000, // 1000 * 60 * 60
+    minute: 60000, // 1000 * 60
+    second: 1000,
+  };
+  const days = Math.floor(timer / miliseconds.day);
+  const hours = Math.floor((timer % miliseconds.day) / miliseconds.hour);
+  const minutes = Math.floor((timer % miliseconds.hour) / miliseconds.minute);
+  const seconds = Math.floor((timer % miliseconds.minute) / miliseconds.second);
+
+  return [days, hours, minutes, seconds];
+};
